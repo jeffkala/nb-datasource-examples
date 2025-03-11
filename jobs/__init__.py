fc225e99989faa06206ed2ba24e234d7660e54c4
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2025-03-11 12:30:13 -0600

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -4,6 +4,7 @@ from nautobot.apps.jobs import register_jobs
 from nautobot_design_builder.util import load_jobs
 
 from .tcp_connectivity_check import ConnectivityCheckTask
+from . command_getter import CommandExecution
 
 load_jobs("jobs")
-register_jobs(ConnectivityCheckTask,)
+register_jobs(ConnectivityCheckTask, CommandExecution)
