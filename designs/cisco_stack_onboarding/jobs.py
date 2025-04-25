23bc324a9c0e88cb784ef4368f241dbba22f0c68
Jeff Kala <jeff.l.kala@gmail.com>
2025-04-25 09:33:04 -0500

    add design to onboard stacks

---
+++ designs/cisco_stack_onboarding/jobs.py
@@ -0,0 +1,22 @@
+"""Initial data required for stacks."""
+
+from nautobot_design_builder.design_job import DesignJob
+from .context import CiscoStackDesignContext
+
+
+class CiscoStackDesign(DesignJob):
+    """Cisco Stack Design."""
+
+    class Meta:
+        """Metadata needed to create a new Cisco stack."""
+
+        name = "Onboard Cisco Stack"
+        description = "Find Stacked devices and create the stack as virtual chassis."
+        commit_default = False
+        design_file = "designs/0001_stack_design.yaml.j2"
+        context_class = CiscoStackDesignContext
+        nautobot_version = ">=2"
+
+    def run(self, dryrun: bool, **kwargs):
+        """Run the job."""
+        super().run(dryrun, **kwargs)
