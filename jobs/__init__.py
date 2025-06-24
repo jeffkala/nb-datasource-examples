7a7d378acab0cc8cb954b9aa428644a8a43bb740
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2025-06-24 12:01:53 -0600

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -5,7 +5,8 @@ from nautobot_design_builder.util import load_jobs
 
 from .tcp_connectivity_check import ConnectivityCheckTask
 from .command_getter import CommandExecution
-from .debug_nornir_inventory import DebugInventoryJob
+# from .debug_nornir_inventory import DebugInventoryJob
 
 load_jobs("jobs")
-register_jobs(ConnectivityCheckTask, CommandExecution, DebugInventoryJob)
+# register_jobs(ConnectivityCheckTask, CommandExecution, DebugInventoryJob)
+register_jobs(ConnectivityCheckTask, CommandExecution)
