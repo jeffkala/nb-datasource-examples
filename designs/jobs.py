cfeeb70e830425754efb2f3f8a704112a1ca177d
Jeff Kala <Jeff.kala@networktocode.com>
2025-07-07 13:22:28 -0600

    add cisco stack

--- designs/jobs.py
+++ designs/jobs.py
@@ -1,4 +1,5 @@
+from .cisco_stack_onboarding.jobs import CiscoStackDesign
 from .ipam_site_design.jobs import IpamSiteDesign
 from .pop_design.jobs import PopDesign
 
-__all__ = ("IpamSiteDesign", "PopDesign")
+__all__ = ("IpamSiteDesign", "PopDesign", "CiscoStackDesign")
