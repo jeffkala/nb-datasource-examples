38f2bdad04e8e85b1ff429c1ddc5fe729395a520
Jeff Kala <Jeff.kala@networktocode.com>
2025-07-08 09:13:11 -0600

    cleanup master logic

--- designs/cisco_stack_onboarding/jobs.py
+++ designs/cisco_stack_onboarding/jobs.py
@@ -1,6 +1,7 @@
 """Initial data required for stacks."""
 
 from nautobot_design_builder.design_job import DesignJob
+
 from .context import CiscoStackDesignContext
 
 
@@ -13,7 +14,11 @@ class CiscoStackDesign(DesignJob):
         name = "Onboard Cisco Stack"
         description = "Find Stacked devices and create the stack as virtual chassis."
         commit_default = False
-        design_file = "designs/0001_stack_design.yaml.j2"
+        # design_file = "designs/0001_stack_design.yaml.j2"
+        design_files = [
+            "designs/0001_stack_design.yaml.j2",
+            "designs/0002_stack_design.yaml.j2",
+        ]
         context_class = CiscoStackDesignContext
         nautobot_version = ">=2"
 
