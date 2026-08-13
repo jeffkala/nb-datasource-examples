58e88caeb152153f130706bc15ce3e77a2107d5e
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2026-08-13 13:03:55 -0600

    Update my_data_compliance_rules.py

--- custom_validators/my_data_compliance_rules.py
+++ custom_validators/my_data_compliance_rules.py
@@ -1,29 +1,25 @@
 import re
-from nautobot_data_validation_engine.custom_validators import DataComplianceRule, ComplianceError
-# from nautobot.data_validation.custom_validators import ComplianceError, DataComplianceRule
-# class ValidateContacts(DataComplianceRule):
-#     model = "extras.contact" # Ex: 'dcim.device'
-#     enforce = False # True/False enforce flag
+from nautobot.apps.models import DataComplianceRule, ComplianceError
 
-#     def audit_name_first_and_last(self):
-#         # Your logic to determine if this function has succeeded or failed
-#         if len(self.context["object"].name.split()) <= 1:
-#             raise ComplianceError({"Name": "Contact must be first and last."})
+class ValidateContacts(DataComplianceRule):
+    model = "extras.contact" # Ex: 'dcim.device'
+    enforce = False # True/False enforce flag
 
-#     # def audit_desired_name_two(self):
-#     #     # Your logic to determine if this function has succeeded or failed
-#     #     if "undesired_value" in self.context["object"].desired_attribute:
-#     #         raise ComplianceError({"desired_attribute": "Desired message why it's invalid."})
+    def audit_name_first_and_last(self):
+        # Your logic to determine if this function has succeeded or failed
+        if len(self.context["object"].name.split()) <= 1:
+            raise ComplianceError({"Name": "Contact must be first and last."})
 
-#     def audit(self):
-#         messages = {}
-#         for fn in [self.audit_name_first_and_last]: # Add audit functions here
-#             try:
-#                 fn()
-#             except ComplianceError as ex:
-#                 messages.update(ex.message_dict)
-#         if messages:
-#             raise ComplianceError(messages)
+    def audit(self):
+        messages = {}
+        for fn in [self.audit_name_first_and_last]: # Add audit functions here
+            try:
+                fn()
+            except ComplianceError as ex:
+                messages.update(ex.message_dict)
+        if messages:
+            raise ComplianceError(messages)
+
 
 class DeviceDataComplianceRules(DataComplianceRule):
     model = "dcim.device"
@@ -31,7 +27,7 @@ class DeviceDataComplianceRules(DataComplianceRule):
     
     # Checks if a device name contains any special characters other than a dash (-), underscore (_), or period (.) using regex
     def audit_device_name_chars(self):
-        if not re.match("^[a-zA-Z0-9\-_.]+$", self.context["object"].name):
+        if not re.match("^[a-zA-Z0-9._-]+$", self.context["object"].name):
             raise ComplianceError({"name": "Device name contains unallowed special characters."})
     
     def audit(self):
